e5e1b49b49987c53e2b9ac8778425008479a8dbd
Pedro Gryzinsky <pedro.gryzinsky@gmail.com>
2016-08-25 13:30:11 -0300

    fixed error on migration: invalid type

--- db/migrate/20160825150936_create_zone_interests.rb
+++ db/migrate/20160825150936_create_zone_interests.rb
@@ -1,9 +1,10 @@
 class CreateZoneInterests < ActiveRecord::Migration
   def change
     create_table :zone_interests do |t|
+      t.integer :spree_zone_id
       t.integer :start_number_of_installments
       t.integer :end_number_of_installments
-      t.numericspree_zone_id :interest
+      t.float :interest
 
       t.timestamps null: false
     end
