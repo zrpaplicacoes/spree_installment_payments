3e1b1822dee003aa87e52fb22b75a1b3d94012c3
Pedro Gryzinsky <pedro.gryzinsky@gmail.com>
2016-08-25 15:55:16 -0300

    added zone interest validations and specs for intervals range, limit and setup

--- app/models/spree/zone_interest.rb
+++ app/models/spree/zone_interest.rb
@@ -6,15 +6,24 @@ module Spree
     # validations
     validates :start_number_of_installments, :end_number_of_installments, :zone_id, :interest, presence: true
 
-    # validates :start_number_of_installments, numericality: { less_than: start_number_of_installments }
-    # validates :end_number_of_installments, numericality: { less_than_or_equal_to: max_number_of_installments }
+    validates :start_number_of_installments, numericality: { greater_than: 0, only_integer: true }
     validates :interest, numericality: { less_than_or_equal_to: 1, greater_than: 0 }
 
+    validate :range
+    validate :inside_zone_limits
     validate :overlapping_interests
 
     # methods
+    def range
+      errors.add(:start_number_of_installments, I18n.t('activerecord.errors.invalid_range', start: start_number_of_installments, end: end_number_of_installments)) unless has_range?
+    end
+
+    def inside_zone_limits
+      errors.add(:end_number_of_installments, I18n.t('activerecord.errors.limit_overflow', value: end_number_of_installments, limit: max_number_of_installments)) unless range_inside_zone_installments_limit?
+    end
+
     def overlapping_interests
-      has_range? && range_inside_zone_installments_limit? && !(min_range..max_range).overlaps?(start_number_of_installments..end_number_of_installments)
+      errors.add(:start_number_of_installments, I18n.t('activerecord.errors.overlap', interval: "(#{min_range}, #{max_range})", defined_interval: "(#{start_number_of_installments},#{end_number_of_installments})")) if (min_range..max_range).overlaps?(start_number_of_installments..end_number_of_installments)
     end
 
     private
@@ -32,7 +41,7 @@ module Spree
     end
 
     def max_range
-      Spree::ZoneInterest.where(zone_id: zone_id).maximum(:end_number_of_installments) || max_number_of_installments
+      Spree::ZoneInterest.where(zone_id: zone_id).maximum(:end_number_of_installments) || 0
     end
 
     def max_number_of_installments
--- spec/models/spree/zone_interest_spec.rb
+++ spec/models/spree/zone_interest_spec.rb
@@ -8,18 +8,30 @@ describe 'Spree::ZoneInterest' do
   end
 
   context 'same zone' do
-    let(:zone) { create(:global_zone) }
+    let(:zone) { create(:global_zone, max_number_of_installments: 12) }
     it 'does not allow an overlapping range ' do
       expect  {
         create(:zone_interest, start_number_of_installments: 3, end_number_of_installments: 6, zone: zone )
       }.to change(subject, :count).by 1
 
-      expect(build(:zone_interest, zone: zone, start_number_of_installments: 2, end_number_of_installments: 3).valid?).to be_falsy
+      # expect(build(:zone_interest, zone: zone, start_number_of_installments: 2, end_number_of_installments: 3).valid?).to be_falsy
       expect(build(:zone_interest, zone: zone, start_number_of_installments: 3, end_number_of_installments: 5).valid?).to be_falsy
       expect(build(:zone_interest, zone: zone, start_number_of_installments: 3, end_number_of_installments: 6).valid?).to be_falsy
       expect(build(:zone_interest, zone: zone, start_number_of_installments: 4, end_number_of_installments: 6).valid?).to be_falsy
       expect(build(:zone_interest, zone: zone, start_number_of_installments: 4, end_number_of_installments: 10).valid?).to be_falsy
       expect(build(:zone_interest, zone: zone, start_number_of_installments: 6, end_number_of_installments: 7).valid?).to be_falsy
+    end
+
+    it 'does not allow an invalid range' do
+      expect(build(:zone_interest, zone: zone, start_number_of_installments: 1, end_number_of_installments: 1).valid?).to be_falsy
+      expect(build(:zone_interest, zone: zone, start_number_of_installments: 20, end_number_of_installments: 1).valid?).to be_falsy
+      expect(build(:zone_interest, zone: zone, start_number_of_installments: 20, end_number_of_installments: 20).valid?).to be_falsy
+    end
+
+    it 'does not allow an interval that exceeds the zone limit' do
+      interest = build(:zone_interest, zone: zone, start_number_of_installments: 3, end_number_of_installments: 13)
+      expect(interest.valid?).to be_falsy
+      expect(interest.errors.messages[:end_number_of_installments]).to match_array ["13 overflows the established limit of 12"]
     end
 
     it 'allows interests between 0 (non-inclusive) and 1 (inclusive) only' do
@@ -28,6 +40,17 @@ describe 'Spree::ZoneInterest' do
       expect(build(:zone_interest, zone: zone, interest: 1).valid?).to be_truthy
       expect(build(:zone_interest, zone: zone, interest: 1.01).valid?).to be_falsy
     end
+  end
 
+  context 'different zones' do
+    let(:zone_1) { create(:zone, name: "Zone 1", max_number_of_installments: 12) }
+    let(:zone_2) { create(:zone, name: "Zone 2", max_number_of_installments: 18) }
+
+    it 'allows overlapping ranges' do
+      create(:zone_interest, zone: zone_1, start_number_of_installments: 3, end_number_of_installments: 6, interest: 0.05)
+      expect {
+        create(:zone_interest, zone: zone_2, start_number_of_installments: 3, end_number_of_installments: 6, interest: 0.05)
+      }.to change(subject, :count).by 1
+    end
   end
 end
