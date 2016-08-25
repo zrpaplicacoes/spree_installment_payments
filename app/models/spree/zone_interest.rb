58c1d8d02472b0db860274839a8c7a0ff968818b
Pedro Gryzinsky <pedro.gryzinsky@gmail.com>
2016-08-25 14:26:44 -0300

    updated zone interest to validate ranges

--- app/models/spree/zone_interest.rb
+++ app/models/spree/zone_interest.rb
@@ -1,7 +1,35 @@
 module Spree
   class ZoneInterest < ActiveRecord::Base
+    # relations
     belongs_to :spree_zone
 
+    # aliasing
     alias_method :zone, :spree_zone
+    delegate :max_number_of_installments, to: :zone
+
+    # validations
+    validates :start_number_of_installments, :end_number_of_installments, :spree_zone_id, :interest, presence: true
+
+    validates :start_number_of_installments, numericality: { less_than: start_number_of_installments }
+    validates :end_number_of_installments, numericality: { less_than_or_equal_to: max_number_of_installments }
+    validates :interest, numericality: { less_than_or_equal_to: 1, greater_than: 0 }
+
+    validate :overlapping_interests
+
+    # methods
+    def overlapping_interests
+      (min_range..max_range).overlaps?(start_number_of_installments..end_number_of_installments)
+    end
+
+    private
+
+    def min_range
+      Spree::ZoneInterest.min(:start_number_of_installments) || 0
+    end
+
+    def max_range
+      Spree::ZoneInterest.max(:end_number_of_installments) || max_number_of_installments
+    end
+
   end
 end
--- lib/spree_installment_payments/factories.rb
+++ lib/spree_installment_payments/factories.rb
@@ -6,7 +6,7 @@ FactoryGirl.define do
     start_number_of_installments 4
     end_number_of_installments 10
     interest 0.0199 # 1.99%
-    zone { create(:zone, max_number_of_installments: 12 ) }
+    spree_zone_id { create(:zone ) }
   end
 
 end
