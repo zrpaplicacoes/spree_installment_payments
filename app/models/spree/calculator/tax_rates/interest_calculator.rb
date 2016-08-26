a910f81dee43a394f2b2705e7c78c959dba17145
Pedro Gryzinsky <pedro.gryzinsky@gmail.com>
2016-08-26 15:35:29 -0300

    using a single #compute method on calculator

--- app/models/spree/calculator/tax_rates/interest_calculator.rb
+++ app/models/spree/calculator/tax_rates/interest_calculator.rb
@@ -6,21 +6,23 @@ module Spree
         Spree.t(:interest_calculator, scope: 'calculators')
       end
 
-      def compute(order=nil)
-        total = order.item_total
-        address = order.billing_address
+      def compute(object)
+        return 0 unless object.respond_to?(:item_total)
+        total = object.item_total
+        address = object.billing_address
         zones = address.country.zones + address.state.zones
 
         if zones.present? && zones.first.max_number_of_installments != 1
-          zone_interest = zones.first.interests.select { |i| order.number_of_installments.between?(i.start_number_of_installments,i.end_number_of_installments) }.first
+          zone_interest = zones.first.interests.select { |i| object.number_of_installments.between?(i.start_number_of_installments,i.end_number_of_installments) }.first
         end
 
         zone_interest ? total * zone_interest.interest : 0
       end
 
-      def available?(order)
-        order.has_installments?
+      def available?(object)
+        object.has_installments?
       end
+
     end
 
   end
