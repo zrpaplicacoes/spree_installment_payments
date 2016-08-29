3ac3d46054c9fb3889005a6be8fdb2faef030ad0
Pedro Gryzinsky <pedro.gryzinsky@gmail.com>
2016-08-29 14:27:30 -0300

    changed base value to decimal

--- db/migrate/20160825190053_add_base_value_for_installment_to_spree_zone.rb
+++ db/migrate/20160825190053_add_base_value_for_installment_to_spree_zone.rb
@@ -1,5 +1,5 @@
 class AddBaseValueForInstallmentToSpreeZone < ActiveRecord::Migration
   def change
-    add_column :spree_zones, :base_value, :float, default: 100.0
+    add_column :spree_zones, :base_value, :decimal, precision: 10, scale: 2, default: 0.0
   end
 end
