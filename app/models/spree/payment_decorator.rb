13ecc7bf6e9c93603f6455d03397e9a01b01bfc0
Pedro Gryzinsky <pedro.gryzinsky@zrp.com.br>
2016-09-09 15:18:57 -0300

    added migration to incude installments to spree credit card model

--- app/models/spree/payment_decorator.rb
+++ app/models/spree/payment_decorator.rb
@@ -1,6 +1,7 @@
 module Spree
 
   Payment.class_eval do
+    after_create :update_source_with_charge_interest
 
     delegate *[:accept_installments?, :max_number_of_installments, :charge_interest ], to: :payment_method
 
@@ -10,8 +11,11 @@ module Spree
 
     def valid_installments?
       interest = Spree::Interest.new(order: order, payment_method: payment_method)
-      byebug
       errors.add(:installments) unless accepted_installment_number?(interest)
+    end
+
+    def update_source_with_charge_interest
+      source.update(charge_interest: payment_method.charge_interest)
     end
 
     def accepted_installment_number?(interest)
