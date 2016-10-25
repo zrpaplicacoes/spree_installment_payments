a694510af0b309b013e84ffed0f504dce72ce723
Pedro Gryzinsky <pedro.gryzinsky@zrp.com.br>
2016-10-25 15:50:05 -0200

    updated namespace

--- app/models/spree/payment/gateway_options_decorator.rb
+++ app/models/spree/payment/gateway_options_decorator.rb
@@ -1,58 +1,55 @@
 module Spree
 
-  class Payment
-    module GatewayOptionsDecorator
-      def hash_methods
-        [
-          :email,
-          :customer,
-          :customer_id,
-          :ip,
-          :order_id,
-          :shipping,
-          :tax,
-          :subtotal,
-          :discount,
-          :currency,
-          :billing_address,
-          :shipping_address,
-          :installments,
-          :charge_interest
-        ]
-      end
+  module Payment::GatewayOptionsDecorator
+    def hash_methods
+      [
+        :email,
+        :customer,
+        :customer_id,
+        :ip,
+        :order_id,
+        :shipping,
+        :tax,
+        :subtotal,
+        :discount,
+        :currency,
+        :billing_address,
+        :shipping_address,
+        :installments,
+        :charge_interest
+      ]
+    end
 
-      def charge_interest
-        @payment.payment_method.charge_interest
-      end
+    def charge_interest
+      @payment.payment_method.charge_interest
+    end
 
-      def installments
-        @payment.installments || 1
-      end
+    def installments
+      @payment.installments || 1
+    end
 
-      private
+    private
 
-      def interest_adjusment
-        if @payment.interest.present? && !@payment.interest.zero?
-          if payment.installments.present? && payment.installments > 1
-            (1.0 + @payment.interest)**payment.installments
-          else
-            (1.0 + @payment.interest)
-          end
+    def interest_adjusment
+      if @payment.interest.present? && !@payment.interest.zero?
+        if payment.installments.present? && payment.installments > 1
+          (1.0 + @payment.interest)**payment.installments
         else
-          1.0
+          (1.0 + @payment.interest)
         end
+      else
+        1.0
       end
-
-      def exchange_multiplier
-        (@payment.payment_method.try(:exchange_multiplier) || 1.0) * interest_adjusment
-      end
-
     end
 
-
-    GatewayOptions.class_eval do
-      prepend Spree::Payment::GatewayOptionsDecorator
+    def exchange_multiplier
+      (@payment.payment_method.try(:exchange_multiplier) || 1.0) * interest_adjusment
     end
+
+  end
+
+  Payment::GatewayOptions.class_eval do
+    prepend Spree::Payment::GatewayOptionsDecorator
   end
 
 end
