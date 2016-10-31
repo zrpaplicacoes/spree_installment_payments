4956fd167b4994533e21ae0a499a9a28186bb817
Pedro Gryzinsky <pedro.gryzinsky@zrp.com.br>
2016-10-31 20:44:44 -0200

    updated order decorator

--- app/models/spree/order_updater_decorator.rb
+++ app/models/spree/order_updater_decorator.rb
@@ -2,8 +2,7 @@ module Spree
   module OrderUpdaterDecorator
 
     def update_order_total
-      current_total = (order.item_total + order.shipment_total + order.adjustment_total)
-      order.total = order.payments.present? && order.payments.last.state == "completed" ?  current_total * order.interest_adjustment : current_total
+      order.total = ( order.item_total + order.shipment_total + order.adjustment_total ) * order.interest_adjustment
     end
 
   end
