83a07ba6322c612ff9fee5345a172f68aeb7d482
Pedro Gryzinsky <pedro.gryzinsky@zrp.com.br>
2016-10-31 21:39:07 -0200

    added update to order total

--- app/models/spree/order_updater_decorator.rb
+++ app/models/spree/order_updater_decorator.rb
@@ -1,6 +1,11 @@
 module Spree
   module OrderUpdaterDecorator
 
+    def update_order_total
+      super
+      order.total = order.total * order.interest_adjustment if !order.payments.nil? && order.payments.last.state == "completed"
+    end
+
     def update_payment_total
       super
       order.payment_total = order.payment_total * order.interest_adjustment
